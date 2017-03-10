fd8c32c78690e243cbf68ab9e8a557f3ed2073a9
Donald Propst <donaldpropst@Donalds-MacBook-Pro.local>
2017-03-10 16:55:24 -0500

    CONTRAST-12765 added server api endpoints and filters

--- lib/contrast/user_api/contrast_sdk.py
+++ lib/contrast/user_api/contrast_sdk.py
@@ -1,5 +1,7 @@
 from util import Util
 from organization_api import _OrganizationApi
+from server_api import _ServerApi
+
 
 class ContrastSdk(object):
 
@@ -29,7 +31,42 @@ class ContrastSdk(object):
 
     def _setup_apis(self):
         self._configure_organization_api()
+        self._configure_server_api()
 
+    def _configure_server_api(self):
+        self._server = _ServerApi()
+        self._configure_api_defaults(self._server)
+        self.get_servers = self._server.get_servers
+        self.get_active_servers = self._server.get_active_servers
+        self.filter_servers = self._server.filter_servers
+        self.get_server_filters = self._server.get_server_filters
+        self.get_server_filter_subfilters = self._server.get_server_filter_subfilters
+        self.get_server_modes = self._server.get_server_modes
+        self.get_server_details = self._server.get_server_details
+        self.get_server_activity = self._server.get_server_activity
+        self.get_server_agent_activity = self._server.get_server_agent_activity
+        self.get_server_attack_status = self._server.get_server_attack_status
+        self.get_server_attack_types = self._server.get_server_attack_types
+        self.get_server_trace_breakdown = self._server.get_server_trace_breakdown
+        self.get_server_trace_severity_breakdown = self._server.get_server_trace_severity_breakdown
+        self.get_server_trace_status_breakdwon = self._server.get_server_trace_status_breakdown
+        self.get_server_libraries_breakdown = self._server.get_server_libraries_breakdown
+        self.update_server_name = self._server.update_server_name
+        self.get_server_properties = self._server.get_server_properties
+        self.get_server_vuln_and_attack_urls = self._server.get_server_vuln_and_attack_urls
+        self.get_server_vuln_urls = self._server.get_server_vuln_urls
+        self.get_server_attack_urls = self._server.get_server_attack_urls
+        self.get_server_libraries = self._server.get_server_libraries
+        self.get_server_libraries_subfilters = self._server.get_server_libraries_subfilters
+        self.filter_server_libraries = self._server.filter_server_libraries
+        self.get_server_trace_subfilters = self._server.get_server_trace_subfilters
+        self.get_server_trace_details = self._server.get_server_trace_details
+        self.filter_server_traces = self._server.filter_server_traces
+        self.delete_server_traces = self._server.delete_server_traces
+        self.get_server_vulnerability_uuids = self._server.get_server_vulnerability_uuids
+        self.get_server_policy_violations = self._server.get_server_policy_violations
+        self.delete_server_trace = self._server.delete_server_trace
+        self.get_server_trace_vulnerability = self._server.get_server_trace_vulnerability
 
     def _configure_organization_api(self):
         self._organization = _OrganizationApi()
