b79ea5ad96aaf8fa6c471c4ad6002ddeb9ceae9c
Gary Moore <gary.moore@contrastsecurity.com>
2020-02-07 12:31:15 -0500

    Prep for 0.14 and enabling Travis

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name='contrast_security',
   packages=['contrast_security', 'contrast_security/filters', 'contrast_security/types', 'contrast_security/user_api'],
-  version='0.13',
+  version='0.14',
   description='Module to easily interact with the Contrast api',
   author='Donnie Propst',
   author_email='donald.propst@contrastsecurity.com',
   url='https://github.com/Contrast-Security-OSS/contrast-sdk-python',
-  download_url='https://github.com/Contrast-Security-OSS/contrast-sdk-python/archive/0.1.tar.gz',
+  download_url='https://github.com/Contrast-Security-OSS/contrast-sdk-python/releases',
   keywords=['contrast', 'security'],
   classifiers=[],
   install_requires=[
