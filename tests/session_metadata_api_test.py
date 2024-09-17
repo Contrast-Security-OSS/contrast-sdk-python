111bbea4651f421baa2d3aaaf6333ed0b3dfb9f0
GowthamMudireddy <90867512+GowthamMudireddy@users.noreply.github.com>
2024-09-17 15:37:13 -0400

    Update session_metadata_api_test.py

--- tests/session_metadata_api_test.py
+++ tests/session_metadata_api_test.py
@@ -17,3 +17,12 @@ class SessionMetadataApiTest(TestCase):
 
     def filter_app_session_metadata_test(self):
         self.assertEqual(200, self.sdk.filter_app_session_metadata(self.org_uuid, self.app_id).status_code)
+
+    def get_app_agentsessions(self):
+        self.assertEqual(200, self.sdk.get_app_agentsessions(self.org_uuid, self.app_id).status_code)
+
+    def get_app_session_metadata_latest(self):
+        self.assertEqual(200, self.sdk.get_app_session_metadata_latest(self.org_uuid, self.app_id).status_code)
+
+    def get_app_session_metadata_sessionid(self):
+        self.assertEqual(200, self.sdk.get_app_session_metadata_sessionid(self.org_uuid, self.app_id, self.session_id).status_code)
