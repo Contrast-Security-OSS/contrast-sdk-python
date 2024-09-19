4cb73c25f2fd69c2566b5f3906bf64f00d1ce00e
GowthamMudireddy <90867512+GowthamMudireddy@users.noreply.github.com>
2024-09-19 09:04:37 -0400

    Update session_metadata_api.py
    fixing based on the review comments

--- contrast_security/user_api/session_metadata_api.py
+++ contrast_security/user_api/session_metadata_api.py
@@ -12,7 +12,7 @@ class _SessionMetadataApi(_ApiSupport):
 
     def get_app_agentsessions(self, org_uuid, app_id):
         path = '{orguuid}/applications/{appId}/agent-sessions'.format(org_uuid=org_uuid, app_id=app_id)
-        return self._get(path, json={})
+        return self._get(path)
 
     def filter_app_session_metadata_filter(self, org_uuid, app_id, limit=None, offset=None):
         path = '{orguuid}/applications/{appId}/agent-sessions/filter'.format(org_uuid=org_uuid, app_id=app_id)
@@ -24,12 +24,12 @@ class _SessionMetadataApi(_ApiSupport):
         if offset is not None:
             params['offset'] = offset
 
-        return self._post(path, json={}, params=params)
+        return self._post(path, params=params)
 
     def get_app_session_metadata_latest(self, org_uuid, app_id):
         path = '{orguuid}/applications/{appId}/agent-sessions/latest'.format(org_uuid=org_uuid, app_id=app_id)
-        return self._get(path, json={})
+        return self._get(path)
 
     def get_app_session_metadata_sessionid(self, org_uuid, app_id, session_id):
         path = '{orguuid}/applications/{appId}/agent-sessions/{sessionId}'.format(org_uuid=org_uuid, app_id=app_id, session_id=session_id)
-        return self._get(path, json={})
+        return self._get(path)
