ab12949ffe57ac6399c32844c3c8a6955ab14a8b
GowthamMudireddy <90867512+GowthamMudireddy@users.noreply.github.com>
2024-09-19 08:52:03 -0400

    Update session_metadata_api.py
    fixed the code as per the review

--- contrast_security/user_api/session_metadata_api.py
+++ contrast_security/user_api/session_metadata_api.py
@@ -11,11 +11,11 @@ class _SessionMetadataApi(_ApiSupport):
         return self._post(path, json={})
 
     def get_app_agentsessions(self, org_uuid, app_id):
-        path = '{orgUuid}/applications/{appId}/agent-sessions'.format(org_uuid=org_uuid, app_id=app_id)
+        path = '{orguuid}/applications/{appId}/agent-sessions'.format(org_uuid=org_uuid, app_id=app_id)
         return self._get(path, json={})
 
     def filter_app_session_metadata_filter(self, org_uuid, app_id, limit=None, offset=None):
-        path = '{orgUuid}/applications/{appId}/agent-sessions/filter'.format(org_uuid=org_uuid, app_id=app_id)
+        path = '{orguuid}/applications/{appId}/agent-sessions/filter'.format(org_uuid=org_uuid, app_id=app_id)
 
         # Prepare query parameters
         params = {}
@@ -27,9 +27,9 @@ class _SessionMetadataApi(_ApiSupport):
         return self._post(path, json={}, params=params)
 
     def get_app_session_metadata_latest(self, org_uuid, app_id):
-        path = '{orgUuid}/applications/{appId}/agent-sessions/latest'.format(org_uuid=org_uuid, app_id=app_id)
+        path = '{orguuid}/applications/{appId}/agent-sessions/latest'.format(org_uuid=org_uuid, app_id=app_id)
         return self._get(path, json={})
 
     def get_app_session_metadata_sessionid(self, org_uuid, app_id, session_id):
-        path = '{orgUuid}/applications/{appId}/agent-sessions/{sessionId}'.format(org_uuid=org_uuid, app_id=app_id, session_id=session_id)
+        path = '{orguuid}/applications/{appId}/agent-sessions/{sessionId}'.format(org_uuid=org_uuid, app_id=app_id, session_id=session_id)
         return self._get(path, json={})
